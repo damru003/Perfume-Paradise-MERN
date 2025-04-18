a9d666d19d90d8c6b2535e1fac21f62fef72bfc4
Deepanshu Sharma <deepanshu30102001@gmail.com>
2025-04-18 14:12:14 +0530

    Update DownloadParadise.jsx

--- frontend/src/components/DownloadPradise/DownloadParadise.jsx
+++ frontend/src/components/DownloadPradise/DownloadParadise.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const DownloadParadise = () => {
   return (
     <>
-        <div className='mt-0 md:mt-10'>
+        <div className='mt-10'>
             <img src={'assets/download_perfume_paradise.jpeg'} className=' w-100 rounded-lg' />
         </div>
     </>
